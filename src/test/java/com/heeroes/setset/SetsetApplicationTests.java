6247ebf23b00f15a34a1ae6428aa36b53f9c3de9
hj-k66 <hjx66@naver.com>
2024-05-12 15:50:30 +0900

    init project

---
+++ src/test/java/com/heeroes/setset/SetsetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heeroes.setset;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetsetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
