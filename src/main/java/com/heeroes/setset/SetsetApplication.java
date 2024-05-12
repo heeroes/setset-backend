2b5370ff10931fdc15a25e44969443d47d8fb56b
hj-k66 <hjx66@naver.com>
2024-05-12 15:50:30 +0900

    init project

---
+++ src/main/java/com/heeroes/setset/SetsetApplication.java
@@ -0,0 +1,13 @@
+package com.heeroes.setset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetsetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SetsetApplication.class, args);
+    }
+
+}
