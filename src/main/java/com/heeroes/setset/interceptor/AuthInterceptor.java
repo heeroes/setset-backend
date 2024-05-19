c8df336ddef12b4d8380f449ef0b965bba2ad46a
heejung Kim <hjx66@naver.com>
2024-05-19 21:00:42 +0900

    hotfix : cors 에러 해결 (#23)
    * hotfix : cors 에러 해결

--- src/main/java/com/heeroes/setset/interceptor/AuthInterceptor.java
+++ src/main/java/com/heeroes/setset/interceptor/AuthInterceptor.java
@@ -19,22 +19,26 @@ public class AuthInterceptor implements HandlerInterceptor{
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		
-		//단순 조회 요청과 preflight 요청인 경우, true 로 넘김
 		String method = request.getMethod();
 		log.debug("AuthInterceptor()의 preHandle실행 method:{}", method);
-		
+		//  preflight 요청인 경우, true 로 넘김
+		if(method.equals("OPTIONS")) return true;
 		// 공유 시, 상세 여행 계획 페이지 접근 가능하도록
 		if(request.getRequestURI().contains("plan/") && method.equals("GET"))
 			return true;
-		
+
 //		if(method.equals("GET") || method.equals("OPTIONS")) return true;
-		
+		request.getHeaderNames().asIterator()
+				.forEachRemaining(headerName -> {
+					String headerValue = request.getHeader(headerName);
+					log.info("Header: {} = {}", headerName, headerValue);
+				});
 		String tokenHeader = request.getHeader("Authorization");	//Header에서 토큰 정보 추출
 		//토큰 헤더가 없거나 Bearer로 시작하지 않는 경우
 		if(tokenHeader == null || !tokenHeader.startsWith("Bearer ")) {
 			response.setStatus(401);
 			response.getWriter().write("Unauthorized");
+			System.out.println("tokenHeader:" + tokenHeader);
 			return false;
 		}
 		//토큰이 유효하지 않은 경우
