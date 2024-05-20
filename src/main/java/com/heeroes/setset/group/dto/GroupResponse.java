1ba164404c1c4172306d8409766d5bf276e34641
heejung Kim <hjx66@naver.com>
2024-05-20 14:15:19 +0900

    feat: 그룹 탈퇴 시, 모두가 그룹을 탈퇴했다면 그룹 내 모든 댓글, 게시글 정보 삭제 (#25)
    & 그룹 정보 조회 시 그룹원 수 조회 정보 추가

--- src/main/java/com/heeroes/setset/group/dto/GroupResponse.java
+++ src/main/java/com/heeroes/setset/group/dto/GroupResponse.java
@@ -1,13 +1,16 @@
 package com.heeroes.setset.group.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class GroupResponse {
     private int id;
     private String name;
+    private int userCnt;
 }
