61347d5c762d4865f0a16ce0ce3be846e1433870
정재희 <wjfwogml5890@naver.com>
2024-05-21 11:51:44 +0900

    #10 재난문자 및 뉴스데이터 제공

--- src/main/java/com/heeroes/setset/safe/dto/DisasterAlarm.java
+++ src/main/java/com/heeroes/setset/safe/dto/DisasterAlarm.java
@@ -1,11 +1,18 @@
 package com.heeroes.setset.safe.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-public class DisasterAlarm {
+@AllArgsConstructor
+public class DisasterAlarm implements Comparable<DisasterAlarm> {
 	String createdAt;  //CRT_DT
 	String message;   //MSG_CN
 	String region;   //RCPTN_RGN_NM
-	String type;     //DST_SE_NM
+	String type;     //EMRG_STEP_NM
+	
+	@Override
+	public int compareTo(DisasterAlarm o) {
+		return -this.createdAt.compareTo(o.createdAt);
+	}
 }
--- src/main/java/com/heeroes/setset/safe/dto/News.java
+++ src/main/java/com/heeroes/setset/safe/dto/News.java
@@ -1,11 +1,19 @@
 package com.heeroes.setset.safe.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-public class News {
+@AllArgsConstructor
+public class News implements Comparable<News>{
 	String title;  //YNA_TTL
 	String content;  //YNA_CN
 	String Date; //YNA_YMD
 	String author; //YNA_WRTR_NM
+	long id; //YNA_NO
+	
+	@Override
+	public int compareTo(News o) {
+		return -this.Date.compareTo(o.Date);
+	}
 }
