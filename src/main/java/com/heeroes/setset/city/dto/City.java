d790b6cf81a8d9b4366f76b4d7a380fc9a28dbb2
jaehee Jung <wjfwogml5890@naver.com>
2024-07-09 20:24:07 +0900

    feat: City dto 생성

---
+++ src/main/java/com/heeroes/setset/city/dto/City.java
@@ -0,0 +1,14 @@
+package com.heeroes.setset.city.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class City {
+	private String sido;
+	private String gu;
+	private String dong;
+}
